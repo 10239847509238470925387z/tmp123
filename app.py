0744394073b4ac3d4392b613706750b838c4546c
10239847509238470925387z <10239847509238470925387z@gmail.com>
2017-02-13 17:32:00 -0500

    bandaid server error again..

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     acct = parameters.get("account-type")
+    acct = acct.strip
     if acct=='401k':
         acct='WI'
 
