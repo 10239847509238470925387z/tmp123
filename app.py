2101f1060078a304683c82fc21d5fb0dce21aa4e
10239847509238470925387z <10239847509238470925387z@gmail.com>
2017-02-13 12:16:20 -0500

    wrong action name fix

--- app.py
+++ app.py
@@ -28,15 +28,15 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    #if req.get("result").get("action") != "shipping.cost":
-    #    return {}
+    if req.get("result").get("action") != "shipping.cost":
+        return {}
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + str(req.get("result").get("action")) + " euros."
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
